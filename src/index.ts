a55983207ecb9be268c600283a6c9af68c2cade3
Trungdev1611 <Trungdev1611@gmail.com>
2023-05-22 17:28:06 +0700

    add swager.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import express from 'express'
 import { Request, Response } from 'express'
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); // https://swagger.io/specification/
+
+
 const app = express()
+
+//show options bar
+var options = {
+    explorer: true
+  };
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 app.get('/', (req:Request, res:Response) => {
     res.status(200).json({
